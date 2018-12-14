f9f8411b754421926895a3b2cf00bf442b56fc97
tarik el bannany <tarik.elbannany@gmail.com>
2018-12-14 13:21:19 +0100

    add screenshot + readme + test fix

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# rain
-object to Excel file based on poi apache project
+# Rain
+### Object to excel spreed sheet
+
+This is an object-oriented java library for writing Microsoft Office Excel spreadsheets.
+Based on apache POI library and the power java reflexion.
+
+#### Create spreadsheet 
+let's give the User.java class
+```java
+ public class User {
+ 
+     @RainRow
+     private String firstName;
+     @RainRow(include = false)
+     private String secondName;
+     @RainRow
+     private String address;
+     @RainRow
+     private String email;
+     @RainRow(name = "PHONE")
+     private String phone;
+     @RainRow(child = {"label"})
+     private City city;
+     
+     // getters and setters
+ 
+ }
+```
+City.java class
+```java
+// lombok way ;)
+ @Data
+ @RequiredArgsConstructor
+ @AllArgsConstructor
+ public class City {
+     private String label;
+     private String description;
+ }
+```    
+Test example
+
+```java
+ public class MySpreadSheetRender {
+     public MySpreadSheetRender() throws IOException {
+          List<User> data = this.data();
+           RainSheet<User> rainSheet = new RainSheet.RainSheetBuilder<User>()
+                         .target(User.class)
+                         .name("test")
+                         .insertDateInName(false)
+                         .rowContent(data)
+                         .build();
+         
+           WorkBookGenerator<User> workBookGenerator = new WorkBookGenerator<>(rainSheet);
+           Workbook workbook = workBookGenerator.render();
+           FileOutputStream out = new FileOutputStream("example.xls");
+                   workbook.write(out);
+                   out.close();
+     }
+ }
+```  
--- src/test/java/net/teb/rain/TestRain.java
+++ src/test/java/net/teb/rain/TestRain.java
@@ -8,6 +8,8 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,21 +21,20 @@ public class TestRain {
         List<User> data = new ArrayList<>();
         data.add(new User("tarik", "tarik2", "address", "tarik.elbannany@gmail.net",
                 "0688559945", new City("city1", "descrtiption")));
-        data.add(new User("tarik", "tarik2", "address", "tarik.elbannany@gmail.net",
-                "0688559945", new City("city1", "descrtiption")));
-        data.add(new User("tarik", "tarik2", "address", "tarik.elbannany@gmail.net",
-                "0688559945", new City("city1", "descrtiption")));
-        data.add(new User("tarik", "tarik2", "address", "tarik.elbannany@gmail.net",
-                "0688559945", new City("city1", "descrtiption")));
-        data.add(new User("tarik", "tarik2", "address", "tarik.elbannany@gmail.net",
-                "0688559945", new City("city1", "descrtiption")));
-        data.add(new User("tarik", "tarik2", "address", "tarik.elbannany@gmail.net",
-                "0688559945", new City("city1", "descrtiption")));
+
+        data.add(new User("amin", "said", "address", "email@gmail.net",
+                "4512785946", new City("city2", "descrtiption2")));
+
+        data.add(new User("issam", "mohammed", "address address", "email2@gmail.net",
+                "0688559945", new City("city3", "descrtiption5")));
+
+        data.add(new User("pierre", "rachid", "address5", "email5@gmail.net",
+                "0559752156", new City("city5", "descrtiption5")));
         return data;
     }
 
     @Test
-    public void testCreation() {
+    public void testCreation() throws IOException {
         List<User> data = this.data();
         RainSheet<User> rainSheet = new RainSheet.RainSheetBuilder<User>()
                 .target(User.class)
@@ -45,6 +46,10 @@ public class TestRain {
         WorkBookGenerator<User> workBookGenerator = new WorkBookGenerator<>(rainSheet);
         Workbook workbook = workBookGenerator.render();
         Assert.assertEquals(workbook.getSheetAt(0).getSheetName(), "test");
-        rainSheet.toString();
+
+        FileOutputStream out = new FileOutputStream("example.xls");
+        workbook.write(out);
+        out.close();
+
     }
 }
